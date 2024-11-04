27a0a7e0f0fc45a01eb74afdf7c7def32e520ee6
HalaChemaa <136089672+HalaChemaa@users.noreply.github.com>
2024-11-04 19:05:45 +0100

    Add files via upload

---
+++ first_program.py
@@ -0,0 +1,4 @@
+# This is your first Python program!
+# It prints a message to the console
+
+print("Hello, World!")
---
+++ using_variables.py
@@ -0,0 +1,16 @@
+# Declaring and assigning values to variables
+"
+name = "Alice"
+"
+age = 25
+"
+city = "New York"
+
+"
+# Using the variables in a sentence
+"
+print("My name is", name + ".")
+"
+print("I am", age, "years old.")
+"
+print("I live in", city + ".")
